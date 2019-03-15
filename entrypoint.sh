992c5505ca4a34c231f3213e3d068844647ab378
Chuang Liu <Chuangliu2016@gmail.com>
2019-03-15 18:06:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
-php-fpm7.0 --daemonize --fpm-config /etc/php/7.0/fpm/php-fpm.conf
-sed -i "s/CLIENT_ID/$CLIENT_ID/g" /etc/v2ray/config.json
-sed -i "s/CLIENT_ALTERID/$CLIENT_ALTERID/g" /etc/v2ray/config.json
-sed -i "s#CLIENT_WSPATH#$CLIENT_WSPATH#g" /etc/v2ray/config.json
-sed -i "s#CLIENT_WSPATH#$CLIENT_WSPATH#g" /etc/nginx/conf.d/default.conf
-v2ray -config=/etc/v2ray/config.json &
-nginx -g 'daemon off;'
+cd /v2raybin
+echo -e "$CONFIG_JSON" > config.json
+if [ "$CERT_PEM" != "$KEY_PEM" ]; then
+  echo -e "$CERT_PEM" > cert.pem
+  echo -e "$KEY_PEM"  > key.pem
+fi
+./v2ray
